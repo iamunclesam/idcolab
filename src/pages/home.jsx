0cf8d95888489d88e89f9bbae15ed46f0d852f94
Unclesam <adeyemis958@gmail.com>
2024-05-10 08:20:24 -0700

    new

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
                             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div class="relative">
                                 <input type="search" id="default-search" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" required />
-                                <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-dark-800 dark:hover:bg-dark-900 dark:focus:ring-blue-800">Start Collaborations</button>
+                                <button type="submit" class="text-white absolute end-2.5 bottom-1.5 bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2 dark:bg-dark-800 dark:hover:bg-dark-900 dark:focus:ring-blue-800">Start Collaborations</button>
                             </div>
                         </form>
 
@@ -39,8 +39,8 @@ const Home = () => {
 
 
                     </div>
-                    <div className="lg:mt-0 lg:col-span-5 lg:flex order-1 md:order-2 " onClick={toggleExpand}>
-                        <img src={imgOne} alt="mockup" className="w-full p-8 md:p-0 animate-breathing infinite" />
+                    <div className="lg:mt-0 lg:col-span-5 lg:flex order-1 md:order-2 " >
+                        <img src={imgOne} alt="mockup" className="w-full p-8 md:p-0 breathing-image" />
                     </div>
                 </div>
             </section>
