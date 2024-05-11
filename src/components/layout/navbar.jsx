b433af0b371b1790175950ede024d16e2ee8be38
Sarah-Obarhoro <obarhorosarah@gmail.com>
2024-05-11 01:10:51 +0100

    Created Navbar and video section

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,47 +1,196 @@
-
+import { useState } from "react";
+import { CgMenuRightAlt } from "react-icons/cg";
+import logo from "../../assets/idcolab.svg";
+import { LuMoon, LuSun } from "react-icons/lu";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 const Navbar = () => {
-    return (
-        <>
-            {/* Demo Navbar */}
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-            <nav className=" z-30 fixed w-full mb-10 px-3 lg:px-16">
-                <div className="bg-white dark:bg-gray-900  max-w-screen-xl flex border lg:border-gray-700 border-gray-300 rounded-full my-6 px-4 flex-wrap items-center justify-between mx-auto py-2">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                       
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">IDCOLAB</span>
-                    </a>
-                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <nav className="z-[200] fixed w-full mb-10 px-3 lg:px-16">
+      {/*bg-blue items-center flex justify-between px-8 h-16*/}
+      <div className="bg-white dark:bg-gray-900  max-w-screen-xl flex border lg:border-gray-700 border-gray-300 rounded-full my-6 px-4 flex-wrap items-center justify-between mx-auto py-2">
+        <a href="/">
+          <img src={logo} alt="idcolab-logo" className="w-40" />
+        </a>
 
-        </>
-    )
-}
+        {/* Styling for larger screens */}
+        <ul className="text-navy hidden lg:flex justify-between gap-9 font-medium pl-60">
+          <li className="">
+            <a href="/">3D MODELS</a>
+          </li>
 
-export default Navbar
+          <li>
+            <a href="/proficiency">CUSTOM 3D SERVICE</a>
+          </li>
+
+          <li>
+            <a href="/projects">FILE VIEWERS</a>
+          </li>
+        </ul>
+
+        <div className="lg:ml-40">
+          <ul className="flex justify-between gap-6">
+                <li>
+                <a href="/blog">
+                    <LuMoon /> <LuSun className="hidden" />
+                </a>
+                </li>
+                <li>
+                <a href="" className="flex">
+                    LOG IN <MdOutlineArrowRightAlt />
+                </a>
+                </li>
+          </ul>
+        </div>
+
+        <button className="border border-black px-5 py-3 rounded-2xl text-center items-center hover:bg-black hover:text-blue hidden lg:flex font-playFair font-medium">
+          <a href="/contact">SIGN UP</a>
+        </button>
+
+        {/* Menu button for small screens */}
+        <button onClick={toggleMenu} className="lg:hidden">
+          <CgMenuRightAlt className="text-3xl" />
+        </button>
+      </div>
+      {/* Nav items for smaller screens */}
+      {menuOpen && (
+        <div className="mt-2 bg-blue rounded pb-8 mx-4">
+          <ul className="text-navy font-playFair font-medium py-4 pl-5">
+            <li className="mb-2">
+              <a href="/">3D MODELS</a>
+            </li>
+
+            <li className="mb-2">
+              <a href="/proficiency">CUSTOM 3D SERVICE</a>
+            </li>
+
+            <li className="mb-2">
+              <a href="/projects">FILE VIEWERS</a>
+            </li>
+
+            <li className="mb-2">
+              <a href="/blog">Blog</a>
+            </li>
+          </ul>
+          <button className="border border-black px-4 py-2 ml-4 rounded-2xl text-center items-center hover:bg-black hover:text-blue font-playFair font-medium text-sm">
+            <a href="/contact">SIGN UP</a>
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
+
+// import logo from "../../assets/idcolab.svg";
+// import { LuMoon, LuSun } from "react-icons/lu";
+// import { MdOutlineArrowRightAlt } from "react-icons/md";
+
+// const Navbar = () => {
+//     const [menuOpen, setMenuOpen] = useState(false);
+
+//     const toggleMenu = () => {
+//       setMenuOpen(!menuOpen);
+//     };
+//     return (
+//       <>
+//         <nav className=" z-30 fixed w-full mb-10 px-3 lg:px-16">
+//           <div className="bg-white dark:bg-gray-900  max-w-screen-xl flex border lg:border-gray-700 border-gray-300 rounded-full my-6 px-4 flex-wrap items-center justify-between mx-auto py-2">
+//             <a
+//               href="/"
+//               className="flex items-center space-x-3 rtl:space-x-reverse"
+//             >
+//               <img src={logo} alt="idcolab-logo" className=" w-40" />
+//             </a>
+//             <button
+//               data-collapse-toggle="navbar-default"
+//               onClick={toggleMenu}
+//               type="button"
+//               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+//               aria-controls="navbar-default"
+//               aria-expanded="false"
+//             >
+//               <span className="sr-only">Open main menu</span>
+//               <svg
+//                 className="w-5 h-5"
+//                 aria-hidden="true"
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 17 14"
+//               >
+//                 <path
+//                   stroke="currentColor"
+//                   stroke-linecap="round"
+//                   stroke-linejoin="round"
+//                   stroke-width="2"
+//                   d="M1 1h15M1 7h15M1 13h15"
+//                 />
+//               </svg>
+//             </button>
+//             <div
+//               className="hidden w-full md:block md:w-auto"
+//               id="navbar-default"
+//             >
+//               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+//                 <li>
+//                   <a
+//                     href="#"
+//                     className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+//                     aria-current="page"
+//                   >
+//                     3D MODELS
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="#"
+//                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                   >
+//                     CUSTOM 3D SERVICE
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="#"
+//                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                   >
+//                     FILE VIEWERS
+//                   </a>
+//                 </li>
+//               </ul>
+//               <div>
+//                 <ul>
+//                   <li>
+//                     <a
+//                       href="#"
+//                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                     >
+//                       <LuMoon /> <LuSun className="hidden" />
+//                     </a>
+//                   </li>
+//                   <li>
+//                     <a
+//                       href="#"
+//                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                     >
+//                       LOG IN <MdOutlineArrowRightAlt />
+//                     </a>
+//                   </li>
+//                 </ul>
+//                 <button>SIGN UP</button>
+//               </div>
+//             </div>
+//           </div>
+//         </nav>
+//       </>
+//     );
+// }
+
+// export default Navbar
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
-import imgOne from '../assets/id-image.svg'
-import Navbar from '../components/layout/navbar.jsx'
+import imgOne from '../assets/id-image.svg';
+import Navbar from '../components/layout/navbar.jsx';
+import Image from '../assets/team.jpg';
+import ReactPlayer from "react-player";
+// import video from '../assets/videos/interior.mp4';
 import '../App.css'
 
 const Home = () => {
@@ -11,43 +14,118 @@ const Home = () => {
     };
 
     return (
-        <>
-            <Navbar />
-            <section className="bg-white dark:bg-gray-900 flex items-center lg:h-screen pt-16 md:px-12">
-                <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-16 lg:py-16 lg:grid-cols-12">
-                    <div className="mr-auto place-self-center lg:col-span-7 order-2 lg:order-1">
-                        <h1 className="lg:max-w-2xl text-center lg:text-left mb-4 my-6 text-3xl font-bold md:text-5xl xl:text-5xl dark:text-white line-height">Revolutionizing Client Collaboration: IDCOLAB for Interior Designers</h1>
-                        <p className="lg:max-w-lg mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-sm dark:text-gray-400 text-xs text-center lg:text-left">Our cloud-based Collaboration tool streamlines the design process for interior designers. Collaborate seamlessly with clients, share ideas, and make decisions in real-time.</p>
+      <>
+        <Navbar />
+        <section className="bg-white dark:bg-gray-900 flex items-center lg:h-screen pt-16 md:px-12">
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-16 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7 order-2 lg:order-1">
+              <h1 className="lg:max-w-2xl text-center lg:text-left mb-4 my-6 text-3xl font-bold md:text-5xl xl:text-5xl dark:text-white line-height">
+                Revolutionizing Client Collaboration: IDCOLAB for Interior
+                Designers
+              </h1>
+              <p className="lg:max-w-lg mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-sm dark:text-gray-400 text-xs text-center lg:text-left">
+                Our cloud-based Collaboration tool streamlines the design
+                process for interior designers. Collaborate seamlessly with
+                clients, share ideas, and make decisions in real-time.
+              </p>
 
-
-                        <form class="lg:max-w-md">
-                            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                            <div class="relative items-center">
-                                <input type="search" id="default-search" class="block w-full h-16 px-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" required />
-                                <button type="submit" class="text-white absolute end-2.5 bottom-2 mt-1.5 bg-gradient-to-br from-purple-700 to-blue-500 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg md:text-md text-sm px-4 py-3.5 dark:bg-dark-800 dark:hover:bg-dark-900 dark:focus:ring-blue-800">Start Collaborations</button>
-                            </div>
-                        </form>
-
-
-                        <p className="flex items-center text-sm text-gray-500 mt-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" className='text-purple-400' width="2em" height="2em" viewBox="0 0 16 16"><path fill="currentColor" d="M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3" /></svg>
-                            For teams & individuals - Web and Mobile
-                        </p>
-
-                        <p className="flex items-center text-sm text-gray-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" className='text-purple-400' width="2em" height="2em" viewBox="0 0 16 16"><path fill="currentColor" d="M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3" /></svg>
-                            Try our forever free plan.No credit card required
-                        </p>
-
-
-                    </div>
-                    <div className="lg:mt-0 lg:col-span-5 lg:flex order-1 lg:order-2 " >
-                        <img src={imgOne} alt="mockup" className="lg:w-full sm:w-full mx-auto p-8 sm:p-10 lg:p-0 breathing-image" />
-                    </div>
+              <form className="lg:max-w-md">
+                <label
+                  for="default-search"
+                  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div className="relative items-center">
+                  <input
+                    type="search"
+                    id="default-search"
+                    className="block w-full h-16 px-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your email"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="text-white absolute end-2.5 bottom-2 mt-1.5 bg-gradient-to-br from-purple-700 to-blue-500 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg md:text-md text-sm px-4 py-3.5 dark:bg-dark-800 dark:hover:bg-dark-900 dark:focus:ring-blue-800"
+                  >
+                    Start Collaborations
+                  </button>
                 </div>
-            </section>
-        </>
-    )
+              </form>
+
+              <p className="flex items-center text-sm text-gray-500 mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-purple-400"
+                  width="2em"
+                  height="2em"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3"
+                  />
+                </svg>
+                For teams & individuals - Web and Mobile
+              </p>
+
+              <p className="flex items-center text-sm text-gray-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-purple-400"
+                  width="2em"
+                  height="2em"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3"
+                  />
+                </svg>
+                Try our forever free plan.No credit card required
+              </p>
+            </div>
+            <div className="lg:mt-0 lg:col-span-5 lg:flex order-1 lg:order-2 ">
+              <img
+                src={imgOne}
+                alt="mockup"
+                className="lg:w-full sm:w-full mx-auto p-8 sm:p-10 lg:p-0 breathing-image"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="videoSection bg-[#E2E8F0] w-auto h-[125vh] flex flex-col pt-12">
+          <div className="relative">
+            <img
+              src={Image}
+              alt=""
+              className="absolute w-[88%] lg:w-[75%] h-[110vh] lg:h-[754px] rounded-3xl ml-[6%] lg:ml-[13%]"
+            />
+            <div className="absolute z-10 bg-gradient-to-r from-[#6628c6] via-[#6628c6] to-[#773AD8] h-[110vh] w-[88%] lg:w-[75%] rounded-3xl ml-[6%] lg:ml-[13%] opacity-95"></div>
+            <div className='absolute rounded-lg overflow-hidden ml-[10.5%] lg:ml-[15%] opacity-100 z-50 w-[300px] md:w-[600px] h-[250px] lg:h-[300px] mt-[6%] lg:mt-[15%]'>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=NoWyNgAQe34&pp=ygUZYWVzdGhldGljIGludGVyaW9yIGRlc2lnbg%3D%3D"
+                controls={true}
+                // width="500px"
+                // height="300px"
+              />
+            </div>
+            <div className="absolute z-50 w-[280px] md:w-[500px] ml-[18%] lg:ml-[50%] mt-[80%] md:mt-[45%] lg:mt-28">
+              <p className=" text-xl md:text-2xl lg:text-3xl text-white" style={{fontFamily: "Space Grotesk"}}>
+                Welcome to IDCOLAB, the ultimate private hub for interior
+                designers to seamlessly connect with their clients and bring
+                effective collaboration to life. Our platform boasts a
+                user-friendly interface and cutting-edge tools, making interior
+                design project collaboration an absolute breeze. Design
+                development is mastered here, and collaboration is done right.
+              </p>
+              <button className='bg-white w-32 md:w-36 h-14 ml-16 md:ml-44 text-center rounded-lg text-lg md:text-xl font-semibold mt-5 md:mt-12 lg:mt-0'>Learn More</button>
+            </div>
+          </div>
+        </section>
+      </>
+    );
 }
 
 export default Home
